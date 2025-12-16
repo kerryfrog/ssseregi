90b2f740e8ab0c90297e461f0ba33fb5e0db5224
Lee Dasom <70635175+kerryfrog@users.noreply.github.com>
2025-12-16 21:38:35 +0900

    write more blog

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -3,7 +3,7 @@ export const AppConfig = {
   title: '유박사의 인생 로드맵 | 사주명리 전문가',
   description:
     '사주명리학을 통해 당신의 잠재력과 인생의 흐름을 분석해 드립니다. 유박사와 함께 자신만의 인생 로드맵을 설계해 보세요.',
-  url: 'https://example.com',
+  url: 'https://gujeolpan.vercel.app/',
   locale: 'ko',
   author: '유박사',
   pagination_size: 10,
